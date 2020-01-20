73554e615191faced0c239e14721a40bd745067e
EugeneChebykin <respublicofrome@gmail.com>
2020-01-20 18:35:45 +0300

    Updates

---
+++ precache-manifest.f86cee835db0f7ad6cc770275ee4c33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ea087ea4a7cead7898c4bae0cc8f05",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "f46306e8eb7dc6f05981",
+    "url": "/blog/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "a004c9e4b572a26d6d05",
+    "url": "/blog/static/css/main.43d092ac.chunk.css"
+  },
+  {
+    "revision": "f46306e8eb7dc6f05981",
+    "url": "/blog/static/js/2.ef35f1e6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/blog/static/js/2.ef35f1e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a004c9e4b572a26d6d05",
+    "url": "/blog/static/js/main.0bcec930.chunk.js"
+  },
+  {
+    "revision": "fa1e3e9ceba80b31f992",
+    "url": "/blog/static/js/runtime-main.08757fe3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FormikAndYupForm/precache-manifest.c3aff5032099851256662dbc1d6092ad.js"
+  "/blog/precache-manifest.f86cee835db0f7ad6cc770275ee4c33f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FormikAndYupForm/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
